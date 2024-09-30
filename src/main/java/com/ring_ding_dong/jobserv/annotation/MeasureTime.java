d7b465bfb5378d4cc21db51af2b30bdb39b875f5
seungju.kim <pipiru100@gmail.com>
2024-09-30 18:20:45 +0900

    init

---
+++ src/main/java/com/ring_ding_dong/jobserv/annotation/MeasureTime.java
@@ -0,0 +1,20 @@
+package com.ring_ding_dong.jobserv.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Description: This annotation is used to measure the time taken by a method to execute.
+ *
+ * @author chimaek
+ * @version 1.0
+ * @since 2024-09-30
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface MeasureTime {
+
+}
