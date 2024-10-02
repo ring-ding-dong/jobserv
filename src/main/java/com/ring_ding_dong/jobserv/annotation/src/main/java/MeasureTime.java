e126bd2e65855f82cec4a429b97a8630e63e89dc
seungju.kim <pipiru100@gmail.com>
2024-10-03 00:24:32 +0900

    MeasureTime 수정 및 JavaDoc 추가

--- src/main/java/com/ring_ding_dong/jobserv/annotation/src/main/java/MeasureTime.java
+++ src/main/java/com/ring_ding_dong/jobserv/annotation/src/main/java/MeasureTime.java
@@ -4,15 +4,64 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Description: This annotation is used to measure the time taken by a method to execute.
+ * Annotation used to measure and log the execution time of a method.
+ * When applied to a method, it will trigger the measurement of the method's execution time
+ * and log the results based on the specified parameters.
+ *
+ * <p>Usage example:
+ * <pre>
+ * {@code
+ * @MeasureTime(value = "importantOperation", logToFile = true, threshold = 100)
+ * public void importantOperation() {
+ *     // Method implementation
+ * }
+ * }
+ * </pre>
  *
  * @author chimaek
  * @version 1.0
  * @since 2024-09-30
  */
-
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface MeasureTime {
 
+    /**
+     * Specifies a custom name for the measured operation.
+     * If not provided, the method name will be used.
+     *
+     * @return the custom name for the measured operation
+     */
+    String value() default "";
+
+    /**
+     * Determines whether the execution time should be logged to the console.
+     *
+     * @return true if logging to console is enabled, false otherwise
+     */
+    boolean logToConsole() default true;
+
+    /**
+     * Determines whether the execution time should be logged to a file.
+     *
+     * @return true if logging to file is enabled, false otherwise
+     */
+    boolean logToFile() default false;
+
+    /**
+     * Specifies the time unit to be used for reporting the execution time.
+     * Possible values are "NANOSECONDS", "MICROSECONDS", "MILLISECONDS", "SECONDS".
+     *
+     * @return the time unit as a string
+     */
+    String timeUnit() default "MILLISECONDS";
+
+    /**
+     * Specifies the minimum execution time (in the specified time unit) required
+     * for the measurement to be logged. Executions faster than this threshold
+     * will not be logged.
+     *
+     * @return the threshold value
+     */
+    double threshold() default 0.0;
 }
