ca1e74836df7bc7997e74aa1722e921146b5c067
seungju.kim <pipiru100@gmail.com>
2024-10-03 00:24:55 +0900

    MeasureTime 수정 및 JavaDoc 추가

--- src/main/java/com/ring_ding_dong/jobserv/annotation/src/main/java/MeasureTime.java
+++ src/main/java/com/ring_ding_dong/jobserv/annotation/src/main/java/MeasureTime.java
@@ -4,9 +4,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation used to measure and log the execution time of a method.
- * When applied to a method, it will trigger the measurement of the method's execution time
- * and log the results based on the specified parameters.
+ * Annotation used to measure and log the execution time of a method. When applied to a method, it
+ * will trigger the measurement of the method's execution time and log the results based on the
+ * specified parameters.
  *
  * <p>Usage example:
  * <pre>
@@ -27,8 +27,8 @@ import java.lang.annotation.Target;
 public @interface MeasureTime {
 
     /**
-     * Specifies a custom name for the measured operation.
-     * If not provided, the method name will be used.
+     * Specifies a custom name for the measured operation. If not provided, the method name will be
+     * used.
      *
      * @return the custom name for the measured operation
      */
@@ -49,17 +49,16 @@ public @interface MeasureTime {
     boolean logToFile() default false;
 
     /**
-     * Specifies the time unit to be used for reporting the execution time.
-     * Possible values are "NANOSECONDS", "MICROSECONDS", "MILLISECONDS", "SECONDS".
+     * Specifies the time unit to be used for reporting the execution time. Possible values are
+     * "NANOSECONDS", "MICROSECONDS", "MILLISECONDS", "SECONDS".
      *
      * @return the time unit as a string
      */
     String timeUnit() default "MILLISECONDS";
 
     /**
-     * Specifies the minimum execution time (in the specified time unit) required
-     * for the measurement to be logged. Executions faster than this threshold
-     * will not be logged.
+     * Specifies the minimum execution time (in the specified time unit) required for the
+     * measurement to be logged. Executions faster than this threshold will not be logged.
      *
      * @return the threshold value
      */
